8e2ac81e96b42712c3c8b423b42128a3aa3b5d1f
AndreIglesias <AndreIglesias@users.noreply.github.com>
2023-02-15 16:05:37 +0000

    Update .env.version with version.py
    Files changed:
    M	.env.vars
    M	client/src/bastionlab/.env.version
    M	client/src/bastionlab/version.py

--- client/src/bastionlab/version.py
+++ client/src/bastionlab/version.py
@@ -1,6 +1,16 @@
 __all__ = [
-    "__bastionlab_version__",
     "__torch_version__",
+    "__version__",
 ]
-__bastionlab_version__ = "0.3.7"
 __torch_version__ = "1.13.1"
+__torch_url__ = (
+    "https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-"
+    + __torch_version__
+    + "%2Bcpu.zip"
+)
+__torch_cxx11_url__ = (
+    "https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-"
+    + __torch_version__
+    + "%2Bcpu.zip"
+)
+__version__ = "0.3.7"
